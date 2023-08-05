dc0cce7229662bc764fbad836b72126f6d01f589
Dwi Fitriana <dwifitriana@jumpa.id>
2023-08-06 00:56:02 +0700

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard'
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className='mt-16 prompt_layout'>
+            {data.map(post => (
+                <PromptCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    )
+}
+
+const Feed = () => {
+    const [searchText, setSearchText] = useState('')
+    const [posts, setPosts] = useState([])
+
+    const handleSearchChange = async () => {}
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const resonse = await fetch('/api/prompt')
+            const data = await resonse.json()
+
+            setPosts(data)
+        }
+
+        fetchPosts()
+    }, [])
+
+    return (
+        <section className='feed'>
+            <form className='relative w-full flex-center'>
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username'
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className='search_input peer'
+                />
+            </form>
+
+            <PromptCardList data={posts} handleTagClick={() => {}} />
+        </section>
+    )
+}
+
+export default Feed
